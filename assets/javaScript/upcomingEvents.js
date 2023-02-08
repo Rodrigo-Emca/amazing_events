80cdb7750e50263bd691387032d602545bb9e4bf
Rodrigo <contactorodrigocabrera@gmail.com>
2023-02-08 14:36:12 -0300

    filtros agregados a upcomingEvents.HTML

--- assets/javaScript/upcomingEvents.js
+++ assets/javaScript/upcomingEvents.js
@@ -1,29 +1,133 @@
 let generalEvents = data.events
 let upcomingEventsCards = document.getElementById("upcomingEventsCards")
 
-
-const upcomingEventsDate = []
-function filtrarPorFecha(fecha, lista){
-    let aux = []
-    for (let event of lista) {
-        if (event.date > fecha){
-            upcomingEventsDate.push(event)
-        }
-    }
-    return aux
-}
 const upcomingEvents = filtrarPorFecha(data.currentDate, generalEvents)
 
-for (let event of upcomingEventsDate) {
-    upcomingEventsCards.innerHTML += `<div class="card text-center col-md-3 m-4" style="width: 18rem;">
-    <img src="${event["image"]}" class="card-img-top p-2" id="imageCard" alt="outing_to_the_museum">
-    <div class="card-body">
-        <h5 class="card-title">${event.name}</h5>
-        <p class="card-text">${event.description}</p>
-        <div class="d-flex justify-content-around">
-        <a href="../html/details.html" class="card-link">Details</a>
-        </div>
+const $checkboxSelection = document.getElementById("checkboxSelection")
+const categoriasRepetidas = generalEvents.map(evento => evento["category"])
+const categoriasSinRepetirSet = new Set (categoriasRepetidas)
+const categoriasSinRepetirArray = [... categoriasSinRepetirSet]
+
+const $keyword = document.getElementById("keyword")
+
+generarCheckbox(categoriasSinRepetirArray, $checkboxSelection)
+
+agregarCard(upcomingEvents, upcomingEventsCards) 
+
+
+//eventos
+
+//CHECKBOX:
+$checkboxSelection.addEventListener("change", () => {
+    upcomingEventsCards.innerHTML = ``
+
+    const filtradosPorCheckbox = document.querySelectorAll(`input[type="checkbox"]:checked`)
+
+    const categorias = getCategories(filtradosPorCheckbox)
+
+    const filtrados=  filtrarCards(upcomingEvents, categorias)
+
+    const palabras = ($keyword.value).toLowerCase()
+
+    const matches = filtrarCoincidencias(filtrados, palabras)
+
+    agregarCard(matches, upcomingEventsCards)
+}
+)
+
+
+//KEYWORD SEARCH:
+
+$keyword.addEventListener("keyup", () => {
+    upcomingEventsCards.innerHTML = ``
+
+const filtradosPorCheckbox = document.querySelectorAll(`input[type="checkbox"]:checked`)
+
+const categorias = getCategories(filtradosPorCheckbox)
+
+const filtrados=  filtrarCards(upcomingEvents, categorias)
+
+const palabras = ($keyword.value).toLowerCase()
+
+const matches = filtrarCoincidencias(filtrados, palabras)
+
+agregarCard(matches, upcomingEventsCards)
+}
+)
+
+  //funciones
+
+function filtrarPorFecha(fecha, lista){
+    const pastEventsDate = []
+    for (let event of lista) {
+        if (event.date > fecha){
+            pastEventsDate.push(event)
+        }
+    }
+    return pastEventsDate
+}
+
+function agregarCard(lista, elementoDestino){
+let template = ""
+for (let evento of lista){
+    template += generarCard(evento)
+}
+elementoDestino.innerHTML += template
+}
+
+function generarCard(evento){
+return `<div class="card text-center col-md-3 m-4" style="width: 18rem;">
+<img src="${evento.image}" class="card-img-top p-2" id="imageCard" alt="outing_to_the_museum">
+<div class="card-body">
+    <h5 class="card-title">${evento.name}</h5>
+    <p class="card-text">${evento.description}</p>
+    <div class="d-flex justify-content-around">
+    <a href="./assets/html/details.html" class="card-link">Details</a>
     </div>
+</div>
 </div>`
 }
 
+function getCategories(nodos){
+const categorias = []
+for(let categoria of nodos){
+    if(categoria) {
+    categorias.push(categoria.value)
+    }    
+}
+return categorias
+}
+
+function filtrarCards(lista, categorias){
+if(categorias.length === 0){
+    return lista
+}
+let aux = []
+for (let evento of lista){
+    for (let categoria of categorias){
+    if (evento.category === categoria){
+        aux.push(evento)
+    }
+    }
+}
+return aux
+}
+
+function filtrarCoincidencias(listaEventos, keywordIngresado) {
+const coincidencias = []
+for (let evento of listaEventos){
+    if((evento["name"].toLowerCase()).includes(keywordIngresado)){
+    coincidencias.push(evento)
+    console.log(evento)
+}
+}
+return coincidencias
+}
+
+function generarCheckbox(lista, elemento){
+for(let categoria of lista) {
+    elemento.innerHTML += `<label class="d-flex flex-wrap gap-1">
+    <input type="checkbox"  name="${categoria}" value="${categoria}">
+    ${categoria}</label>` 
+}
+}
