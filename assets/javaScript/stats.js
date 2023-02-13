c1d73480d52f8d74c46107ebc13ab4e417edf13a
Rodrigo <contactorodrigocabrera@gmail.com>
2023-02-12 23:57:19 -0300

    stats.js data a partir de API. Primera tabla generada.

--- assets/javaScript/stats.js
+++ assets/javaScript/stats.js
@@ -1,4 +1,4 @@
-import {agregarCard, generarCheckbox, getCategories, filtrarCards, filtrarCoincidencias} from "./module/functions.js"
+import {agregarCard, generarCheckbox, getCategories, filtrarCards, filtrarCoincidencias, filtrarPorFechaPasada} from "./module/functions.js"
 
 const firstTable = document.getElementById("firstTable")
 const secondTable = document.getElementById("secondTable")
@@ -7,10 +7,12 @@ const thirdTable = document.getElementById("thirdTable")
 fetch('https://mindhub-xj03.onrender.com/api/amazing')
   .then(response => response.json() )
   .then(datos => {
-    console.log(datos.events)
+    let eventosPasados = filtrarPorFechaPasada(datos.currentDate, datos.events)
 
-    console.log(porcentajeMasAlto(datos.events))
-
+    crearPrimeraTabla(porcentajeMasAlto(eventosPasados), porcentajeMasBajo(eventosPasados),eventoMayorCapacidad(datos.events))
+    //console.log(porcentajeMasAlto(eventosPasados))
+    //console.log(porcentajeMasBajo(eventosPasados))
+    //console.log(eventoMayorCapacidad(datos.events))
 
   })
   .catch(error => console.log(error))
@@ -19,8 +21,51 @@ fetch('https://mindhub-xj03.onrender.com/api/amazing')
     function porcentajeMasAlto(array){
         let aux = []
         array.forEach(element => {
-            let promedio = element.assistance *100 / element.capacity
-            aux.push(promedio)
+          let promedio =(element.assistance / element.capacity *100)
+          element.promedio = promedio
+          aux.push(element)
         });
-        return aux
+        let auxMax = aux.sort(function(a, b){return b.promedio - a.promedio}).slice([0],[1])
+        let auxCompl = auxMax[0].name + " " + auxMax[0].promedio + "%"
+        return auxCompl
     }
+
+    function porcentajeMasBajo(array){
+      let aux = []
+      array.forEach(element => {
+        let promedio = (element.assistance / element.capacity *100)
+        element.promedio = promedio
+        aux.push(element)
+      });
+      let auxMax = aux.sort(function(a, b){return a.promedio - b.promedio}).slice([0],[1])
+      let auxCompl = auxMax[0].name + " " + auxMax[0].promedio + "%"
+      return auxCompl
+  }
+
+  function eventoMayorCapacidad(array){
+    let aux = array.sort(function(a, b){return b.capacity - a.capacity}).slice([0],[1])
+    let auxCompl = aux[0].name + " " + aux[0].capacity
+    return auxCompl
+}
+
+  function crearPrimeraTabla(evento1, evento2, evento3){
+    firstTable.innerHTML = `<table class=" container border border-secondary" >
+    <thead class="border border-secondary bg-dark-subtle">
+        <tr>
+            <th colspan="3">Events Statistics</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="col-4 border border-secondary">Events with the highest percentage of attendance</td>
+            <td class="col-4 border border-secondary">Events with the lowest percentage of attendance</td>
+            <td class="col-4 border border-secondary">Event with larger capacity</td>
+        </tr>
+        <tr>
+            <td class="col-4 border border-secondary text-center">${evento1}</td>
+            <td class="col-4 border border-secondary text-center">${evento2}</td>
+            <td class="col-4 border border-secondary text-center">${evento3}</td>
+        </tr>
+    </tbody>
+</table>`
+  }
